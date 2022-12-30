4e5f459c188d35bedb84d5428741ce7214d9f2d9
ghassewooof <121681428+ghassewooof@users.noreply.github.com>
2022-12-30 17:31:00 +0100

    Add files via upload

---
+++ python/ghassen.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+print("hello ")
+sleep(1)
+x = "hi"
+
+print(x)
+sleep(1)
+z = "how are you"
+
+print(z)
+sleep(1)
+x = input("What's your name : ")
+
+print("Nice to meet you ", x)
